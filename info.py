7f7494c85bd304705887e19d6b1d2f7d1ffa8c24
ThanzeerCould1 <115057527+ThanzeerCould1@users.noreply.github.com>
2023-02-16 00:33:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -38,10 +38,10 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "raziBotV1")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # FSUB
-auth_channel = environ.get('AUTH_CHANNEL','-1001688834351')
+auth_channel = environ.get('AUTH_CHANNEL','-1001701438445')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 # Set to False inside the bracket if you don't want to use Request Channel else set it to Channel ID
-REQ_CHANNEL = environ.get('REQ_CHANNEL', "-1001688834351")
+REQ_CHANNEL = environ.get('REQ_CHANNEL', "-1001701438445")
 REQ_CHANNEL = int(REQ_CHANNEL) if REQ_CHANNEL and id_pattern.search(REQ_CHANNEL) else False
 JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DATABASE_URI)
 
